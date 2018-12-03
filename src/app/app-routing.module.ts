fd00410bd4368ded4a801ac07d3a2b2fa1cc11fd
ahmadjaani <raza.ahmad.khan786@gmail.com>
2018-12-03 15:57:36 +0530

    commit 3-dec

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {SettingsComponent} from './settings/settings.component';
+import { ErrorComponent  } from './error/error.component';
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '', redirectTo: 'HomeComponent', pathMatch: 'full'},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
